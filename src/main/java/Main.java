23f870a310830bae2a9ade33bdc26e3591cd8bb1
Sagar02k4 <dev.sagar2k4@gmail.com>
2026-07-04 23:09:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+
         
         while(true){
             System.out.print("$ ");
-            String command = sc.nextLine();
+            String input = sc.nextLine();
+
+            String command = input.indexOf(" ") == -1 ? input : input.substring(0, input.indexOf(" "));
+            String remainder = input.indexOf(" ") == -1 ? "" : input.substring(input.indexOf(" ") + 1);
             if(command.startsWith("echo ")){
                 System.out.println(command.substring(5));
             }
             else if(command.equals("exit")){
                 break;
+            }
+            else if(command.equals("type")){
+                if(remainder.equals("echo") || remainder.equals("exit") || remainder.equals("type")){
+                    System.out.println(remainder + " is a shell builtin");
+                }
+                else{
+                    System.out.println(remainder + ": command not found");
+                }
             }
             else{
                 System.out.println(command + ": command not found");
